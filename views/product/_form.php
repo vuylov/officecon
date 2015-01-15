6d7202fd45ba689633a58b2984922d0ebc63a39d
vuylov <vuylov@gmail.com>
2015-01-15 16:09:07 +0300

    Added little fixes

--- views/product/_form.php
+++ views/product/_form.php
@@ -6,6 +6,7 @@ use yii\helpers\Html;
 use app\models\Catalog;
 use app\models\Manufacturer;
 use app\models\Product;
+use vova07\imperavi\Widget;
 ?>
 
 <?php $form = ActiveForm::begin([
@@ -14,7 +15,16 @@ use app\models\Product;
 ]);?>
 
 <?= $form->field($product, 'name');?>
-<?= $form->field($product, 'description')->textarea();?>
+
+<?//= $form->field($product, 'description')->textarea();?>
+<?= $form->field($product, 'description')->widget(Widget::className(),[
+    'settings' => [
+        'lang'         => 'ru',
+        'minHeight'     => 200,
+        'pastePlainText'=> true,
+        'plugins'   => ['table','fullscreen','video', 'fontsize', 'fontcolor']
+    ]
+]);?>
 <?= $form->field($product, 'user_id');?>
 <?= $form->field($product, 'parent_id')->dropDownList(ArrayHelper::map(Product::find()->where('parent_id IS NULL')->all(),'id','name'));?>
 <?= $form->field($product, 'manufacturer_id')->dropDownList(ArrayHelper::map(Manufacturer::find()->all(), 'id', 'name'));?>
