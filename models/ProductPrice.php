e33ee76194627cfc69b8f145d5885d894e55d672
vuylov <vuylov@gmail.com>
2015-01-18 12:46:43 +0300

    Generated model for ProductPrice

---
+++ models/ProductPrice.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "productPrice".
+ *
+ * @property integer $id
+ * @property integer $productItem_id
+ * @property integer $currency_id
+ * @property double $value
+ * @property string $cause
+ *
+ * @property Currency $currency
+ * @property ProductItem $productItem
+ */
+class ProductPrice extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'productPrice';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['productItem_id', 'currency_id'], 'required'],
+            [['productItem_id', 'currency_id'], 'integer'],
+            [['value'], 'number'],
+            [['cause'], 'string']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'productItem_id' => Yii::t('app', 'Product Item ID'),
+            'currency_id' => Yii::t('app', 'Currency ID'),
+            'value' => Yii::t('app', 'Value'),
+            'cause' => Yii::t('app', 'Cause'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCurrency()
+    {
+        return $this->hasOne(Currency::className(), ['id' => 'currency_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProductItem()
+    {
+        return $this->hasOne(ProductItem::className(), ['id' => 'productItem_id']);
+    }
+}
