e91a13f05167e103e75e2bf66dc2e216f7f17e23
vuylov <vuylov@gmail.com>
2015-01-23 13:22:59 +0300

    Created migration for File model

---
+++ migrations/m150123_101748_create_table_file.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150123_101748_create_table_file extends Migration
+{
+    public function up()
+    {
+        $this->createTable('file', [
+            'id'            => 'pk',
+            'fid'           => Schema::TYPE_INTEGER.' NOT NULL',
+            'type'          => Schema::TYPE_STRING.' NOT NULL',
+            'path'          => Schema::TYPE_STRING.' NOT NULL',
+            'thumbnail'     => Schema::TYPE_STRING,
+            'extension'     => Schema::TYPE_STRING,
+            'create_at'     => Schema::TYPE_DATETIME
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('file');
+    }
+}
