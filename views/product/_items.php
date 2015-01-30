bd216a144fbff4bab6aa88dc3e39107a1163e0fb
vuylov <vuylov@gmail.com>
2015-01-30 16:03:59 +0300

    Fixes _items view

--- views/product/_items.php
+++ views/product/_items.php
@@ -5,15 +5,19 @@ use yii\helpers\Url;
 
 <div class="product-items">
     <?php foreach($model->childs as $child):?>
-        <a href="<?=Url::to(['catalog/view', 'id' => $child->catalog->id, 'product'=> $child->id]) ?>"
-            <div class="item">
-                <?php if($child->files):?>
-                    <?php $img = $child->files;?>
-                    <img src = '<?= Yii::$app->homeUrl.'/'.$img[0]->path;?>' width="33%">
-                <?php endif;?>
-                <?= $child->name ?>
-            </div>
-        </a>
+            <a href="<?=Url::to(['catalog/view', 'id' => $child->catalog->id, 'product' => $child->id]); ?>" style="display: block">
+                <div class="item">
+                    <?php if($child->files):?>
+                        <?php $img = $child->files;?>
+                        <div class="image-item"><img src = '<?= Yii::$app->homeUrl.'/'.$img[0]->path;?>' width="33%"></div>
+                    <?php endif;?>
+                    <br>
+                    <span class="item-name"><?= $child->name ?></span>
+                    <br>
+                    <span class="item-name"><?= $child->article ?></span>
+                </div>
+            </a>
+
     <?php endforeach;?>
 </div>
 <div class="clearfix"></div>
