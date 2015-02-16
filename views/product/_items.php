200c18f764e3b8cf10b2736f453a8cd673e3c95e
vuylov <vuylov@gmail.com>
2015-02-16 14:53:30 +0300

    Smaller size for items

--- views/product/_items.php
+++ views/product/_items.php
@@ -4,11 +4,11 @@ use yii\helpers\Url;
 ?>
 <div class="row">
     <?php foreach($model->childs as $child):?>
-        <div class="col-xs-6 col-md-3">
+        <div class="col-xs-6 col-md-2">
             <a href="<?=Url::to(['catalog/view', 'id' => $child->catalog->id, 'product' => $child->id]); ?>" class="thumbnail">
                 <?php if(count($child->files) > 0):?>
                     <?php $img = $child->files;?>
-                    <img src = '<?= Yii::$app->homeUrl.$img[0]->path;?>' style="height: 150px; width: 100%; display: block">
+                    <img src = '<?= Yii::$app->homeUrl.$img[0]->path;?>' style="height: 100px; width: 100%; display: block">
                     <div class="small product-notice">
                         <p><?=($child->name) ? $child->name : '';?></p>
                         <p><?=($child->article) ? $child->article : '';?></p>
