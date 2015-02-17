2edafed3989e1a89a68f2fbe05556612e9173f56
vuylov <vuylov@gmail.com>
2015-02-17 13:33:37 +0300

    fixed untracked files

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=officecon',
-    'username' => 'officeconuser',
-    'password' => '0fficeC0n2o15',
-    'charset' => 'utf8',
-];
